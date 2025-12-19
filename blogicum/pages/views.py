c9a3f877702b94d338c0d2dcb0f998c6f95d739e
pusheen1024 <rita26-10@mail.ru>
2025-12-19 20:38:39 +0400

    feat: add custom error pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def about(request):
-    template = 'pages/about.html'
-    return render(request, template)
+def handler403(request):
+    template = 'pages/403csrf.html'
+    return render(request, template, status=403)
 
 
-def rules(request):
-    template = 'pages/rules.html'
-    return render(request, template)
+def handler404(request):
+    template = 'pages/404.html'
+    return render(request, template, status=404)
+
+
+def handler500(request):
+    template = 'pages/500.html'
+    return render(request, template, status=500)
+
+
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
+
+
+class RulesView(TemplateView):
+    template_name = 'pages/rules.html'
