a609da0664121a224195f56311307b7100c8fa07
pusheen2048 <pusheen-1024@yandex.ru>
2025-12-23 22:04:59 +0400

    chore: add comment model to admin

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from blog.models import Category, Location, Post
+from blog.models import Category, Location, Post, Comment
 
 admin.site.register(Category)
 admin.site.register(Location)
 admin.site.register(Post)
+admin.site.register(Comment)
